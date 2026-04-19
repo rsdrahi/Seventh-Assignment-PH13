8569010b30e195d63995386c9ced47502e8f5ae7
rsdrahi <rsdrahi13@gmail.com>
2026-04-19 18:05:58 +0600

    loading spinner added.

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,5 +1,6 @@
 import React, { use, useEffect, useState } from 'react';
 import FriendsCard from './FriendsCard';
+import { RingLoader } from 'react-spinners';
 
 // const friendsPromise = fetch("/data.json")
 //   .then((res) => res.json());
@@ -9,18 +10,24 @@ const Friends = () => {
   // const friends = use(friendsPromise);
   // console.log(friends, "friends");
   const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const friendsData = async () => {
       const res = await fetch("/data.json")
       const data = await res.json();
       console.log(data, "data");
-      setFriends(data)
+      setFriends(data);
+      setTimeout(() => {
+        setFriends(data);
+        setLoading(false);
+      }, 1000)
     };
     friendsData();
   }, [])
 
-  console.log(friends, "friends")
+  console.log(friends, "friends");
+  console.log(loading, "loading");
 
   return (
     <div className='container mx-auto mb-6'>
@@ -28,11 +35,15 @@ const Friends = () => {
 
       Total Friends:  {friends.length} 
       <div className='grid grid-cols-4 gap-6'>
-      {
-          friends.map((friend, ind) => <FriendsCard
+        {loading ? (
+          <div className='flex justify-center items-center col-span-4 min-h-40'>
+            <RingLoader color='#244D3F'/>
+          </div>
+          ) :
+          (friends.map((friend, ind) => <FriendsCard
             key={ind} friend={friend}>
-            </FriendsCard>)
-      }
+            </FriendsCard>))
+          }
       </div>
     </div>
   );
--- src/components/Friends/FriendsCard.jsx
+++ src/components/Friends/FriendsCard.jsx
@@ -4,23 +4,23 @@ const FriendsCard = ({ friend }) => {
 
   const { name, picture, status, tags, days_since_contact } = friend
   const statusFormat =
-    status === "overdue" ? "bg-red-500" : status === "almost due" ? "bg-yellow-500" : "bg-green-500";
+    status === "overdue" ? "bg-[#EF4444]" : status === "almost due" ? "bg-[#EFAD44]" : "bg-[#244D3F]";
   
   return (
     <div className='shadow rounded-xl text-center space-y-2 p-4 bg-white mt-6'>
       <img src={picture} alt="" className='rounded-full mx-auto' />
       <h2 className='font-bold text-2xl'>{name}</h2>
-      <p className='text-[#64748B] text-sm'>{days_since_contact}d ago</p>
+      <p className='text-[#64748B] text-xl'>{days_since_contact}d ago</p>
       <div className='flex justify-center gap-2 items-center'>
         {
           tags.map((tag, ind) => (<span key={ind}
-            className='bg-green-200 rounded-full px-2 py-1 text-[#244D3F]'>
-            {tag}</span>))
+            className='bg-green-200 rounded-full px-3 py-2 text-[#244D3F] font-semibold'>
+            {tag.toUpperCase()}</span>))
         }
       </div>
       <div className='flex justify-center text-white'>
-         <p className={`${statusFormat} rounded-full py-1 w-24`}>
-        {status}
+         <p className={`${statusFormat} rounded-full py-1 w-24 capitalize`}>
+        {status.toLowerCase()}
       </p>
      </div>
     </div>
